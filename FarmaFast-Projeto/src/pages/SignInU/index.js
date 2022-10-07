dce34744ae1add2fec44ed8bede87642154a605d
Isabella Damião <belladamiao2@gmail.com>
2022-10-07 18:46:02 -0300

    Init commit

---
+++ FarmaFast-Projeto/src/pages/SignInU/index.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { View, Text, StyleSheet, Alert, Image, TextInput, TouchableOpacity } from 'react-native';
+import * as Animatable from 'react-native-animatable'
+
+export default function SignInU() {
+    return (
+
+        <View style={styles.container}>
+        <View style={styles.containerLogo}>
+        <Image
+            source={require('../../assets/logoCircle.png')}
+            style={{width:'60%'}}
+            resizeMode='contain'
+        />
+    </View>
+    
+        <Animatable.View animation="fadeInUp" styles={styles.containerForm}>
+
+        <Text style={styles.title}>CPF</Text>
+        <TextInput
+        style={styles.input}
+        placeholder="000.000.000-00"
+        />
+
+        <Text style={styles.title}>Senha</Text>
+        <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="Digite sua senha"
+        />
+
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Entrar</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonRegister}>
+                <Text style={styles.registerText}>Não possui uma conta? Cadastre-se</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonForgot}>
+                <Text style={styles.forgotText}>Esqueci minha senha</Text>
+            </TouchableOpacity>
+
+    
+</Animatable.View>
+
+</View>
+ 
+)
+}
+
+     const styles = StyleSheet.create({
+        container:{
+            flex:1,
+            backgroundColor:'#83c4ec'
+        },
+        containerLogo:{
+            flex:0.7,
+            justifyContent:'center',
+            alignItems:'center',
+        },
+        containerForm:{
+            backgroundColor:'#fff',
+            flex:1,
+            borderTopLeftRadius:25,
+            borderTopRightRadius:25,
+            paddingStart:'10%',
+            paddingEnd:'5%'
+        },
+        title:{
+          fontSize:20,
+          marginTop:5,
+          marginLeft:'10%',
+        },
+        input:{
+            marginTop: 5,
+            marginLeft:'10%',
+            padding: 15,
+            width: 300,
+            backgroundColor:'#e0e1e6',
+            fontSize:16,
+            fontWeight:'bold',
+            borderRadius:3
+        },
+        button:{
+            backgroundColor:'#4eabe4',
+            width:'80%',
+            alignItems:'center',
+            borderRadius:4,
+            marginLeft:'10%',
+            paddingVertical: 10,
+            justifyContent:'center',
+            marginTop:15,
+        },
+        buttonText:{
+            color:'#fff',
+            fontSize:18,
+            fontWeight:'bold'
+        },
+        buttonRegister:{
+            marginTop:14,
+            alignSelf:'center',
+        },
+        registerText:{
+           color:'#e0e1e6',
+           fontSize:17,
+           fontWeight:'bold'
+        },
+        buttonForgot:{
+            marginTop:24,
+            alignSelf:'center',
+        },
+        forgotText:{
+        color:'#e0e1e6',
+           fontSize:15,
+           fontWeight:'bold'
+        }
+    })
