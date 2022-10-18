5337910694bf90113edd952551e1dc69a1e93c8b
vitgmcorrea <vitoria_099@hotmail.com>
2022-10-18 19:54:44 -0300

    Tela RegisterUser

--- FarmaFast-Projeto/views/importsTelas.js
+++ FarmaFast-Projeto/views/importsTelas.js
@@ -2,5 +2,6 @@ import Inicial from './Inicial';
 import LoginUsuario from './LoginUsuario';
 import LoginFarmacia from './LoginFarmacia';
 import RegisterPharma from './RegisterPharma';
- 
-export {Inicial, LoginFarmacia, LoginUsuario, RegisterPharma}
+import RegisterUser from './RegisterUser';
+
+export {Inicial, LoginFarmacia, LoginUsuario, RegisterPharma, RegisterUser}
