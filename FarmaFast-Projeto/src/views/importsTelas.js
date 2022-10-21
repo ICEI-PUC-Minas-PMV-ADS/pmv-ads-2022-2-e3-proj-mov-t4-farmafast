a82ddb1d62ace51994be622689a7a3330faf955a
arthurvictorprogm <arthurvictorprogm@gmail.com>
2022-10-21 00:48:48 -0300

    update

--- FarmaFast-Projeto/src/views/importsTelas.js
+++
@@ -1,7 +0,0 @@
-import Inicial from './Inicial';
-import LoginUsuario from './LoginUsuario';
-import LoginFarmacia from './LoginFarmacia';
-import RegisterPharma from './RegisterPharma';
-import RegisterUser from './RegisterUser';
-
-export {Inicial, LoginFarmacia, LoginUsuario, RegisterPharma, RegisterUser}
