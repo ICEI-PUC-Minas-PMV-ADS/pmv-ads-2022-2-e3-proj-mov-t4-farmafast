3426528525acd91e1d62eb5335b1d7d6814350bb
Júlia Mendes <juliamendesllima@gmail.com>
2022-11-16 22:11:06 -0300

    Organização

---
+++ FarmaFast-Projeto/views/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
+
+
+const Header = () => (
+    <View>
+        <Appbar.Header style={[styles.header]}>
+            <Appbar.BackAction onPress={() => { }} />
+            <Appbar.Content title={" "} />
+            <Appbar.Action icon="magnify" onPress={() => { }} />
+        </Appbar.Header>
+    </View>
+);
+
+
+const styles = StyleSheet.create({
+    header: {
+        flex: 1,
+        backgroundColor: '#4eabe4'
+    }
+})
+
+export default Header;
+
---
+++ FarmaFast-Projeto/views/importsComponents.js
@@ -0,0 +1,7 @@
+
+import Header from './components/Header';
+import Container from './components/Container';
+import ContainerLogo from './components/ContainerLogo';
+
+export {Header, Container, ContainerLogo};
+
